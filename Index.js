668744939ea1fd43158bbb56b307fead702ccfc3
RECSTEINS <raulcedeno_manzanarez@hotmail.com>
2024-03-03 10:52:17 -0500

    Cambio 03/03/24

--- Index.js
+++ Index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require("express");
+const cors    = require("cors");
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 app.use(require('./routes/proveedores'));
 app.use(require('./routes/productos'));
